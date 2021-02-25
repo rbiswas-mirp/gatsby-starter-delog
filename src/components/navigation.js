d79a7714e3eb059fe70019d4e54973274d3b9965
rbiswas-mirp <biswasrahul016@gmail.com>
2021-02-25 13:16:14 +0530

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+    <Link to="/submit-confession/">Submit Confession</Link>
     <ThemeChanger/>
   </nav>
   
