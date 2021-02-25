00a38dac3362f40a804b576cf7933d3e5162e321
rbiswas-mirp <biswasrahul016@gmail.com>
2021-02-25 14:55:25 +0530

    Update submit-confession.js

--- src/pages/submit-confession.js
+++ src/pages/submit-confession.js
@@ -17,7 +17,7 @@ const ContactPage = ({
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Submit a confession</h1>
-          <p>Don't worry, your name and email will be kept confidential. I need this for combating spam submissions</p>
+          <p>Don't worry, your name and email will be kept confidential. I need this info for combating spam submissions</p>
         </div>
         <div>
           <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
