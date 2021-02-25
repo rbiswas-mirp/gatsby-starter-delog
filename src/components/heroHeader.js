8d9519f741333f7d04c4f0c12bfa2ecf2c83f620
rbiswas-mirp <biswasrahul016@gmail.com>
2021-02-25 13:20:51 +0530

    Update heroHeader.js

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,7 +21,7 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/contact' className="button -primary">Submit a confession &rarr;</Link>
+        <Link to='/submit-confession/' className="button -primary">Submit a confession &rarr;</Link>
       </div>
     )}
   />
